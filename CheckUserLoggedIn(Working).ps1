eabc9968724e7aec327e60a049dd47161ddb1bc6
SamBlakeC <44275251+SamBlakeC@users.noreply.github.com>
2019-04-12 14:51:22 +0100

    Add files via upload

---
+++ CheckUserLoggedIn(Working).ps1
@@ -0,0 +1,8 @@
+﻿$queryResults = (qwinsta /server:$ServerName | foreach { (($_.trim() -replace "\s+",","))} | ConvertFrom-Csv)
+$loggedin = 'false'
+ForEach ($queryResult in $queryResults) {
+if($queryResult.ID -eq 'Disc' -and $queryResults.SESSIONNAME -ne 'services'){
+$loggedin = 'true'
+}
+}
+$loggedin
